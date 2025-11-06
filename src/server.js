b40ae7159fbc5eb708286a3809b5dc2efe17951a
AliHaiderr609 <alih659934@gmail.com>
2025-11-06 09:55:53 -0800

    update my github configration

--- src/server.js
+++ src/server.js
@@ -87,7 +87,6 @@ const startServer = async () => {
     // Connect to Redis
     await connectRedis();
     
-    // Start Express server
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
       console.log(`API Documentation: http://localhost:${PORT}/api-docs`);
